3b0cf1bbe739fabfca6a7d95e1cde6d5d675e5b2
vamsi34 <vamsim7989@gmail.com>
2024-07-02 23:24:48 +0530

    updated

--- PrimeNum.java
+++ PrimeNum.java
@@ -1,19 +1,23 @@
 class PrimeNum {
     public static void main(String args[]) {
-        int num = 179;
+        int num = 29; // Example number to check
         boolean isPrime = true;
 
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                System.out.println(i);
-                // break;  // exit the loop if a factor is found
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;  // exit the loop if a factor is found
+                }
             }
         }
-        // if (isPrime) {
-        //     System.out.println("\nprime number");
-        // } else {
-        //     System.out.println("\nnot prime");
-        // }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
     }
 }
