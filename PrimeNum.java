808815c430c947c40894609e9b13ab148e2eb557
vamsi34 <vamsim7989@gmail.com>
2024-07-06 23:14:31 +0530

    new

--- PrimeNum.java
+++ PrimeNum.java
@@ -21,3 +21,45 @@ class PrimeNum {
         }
     }
 }
+
+// Example: Checking if 29 is a Prime Number
+// Using num / 2
+// If we check up to num / 2, we would iterate through the following steps:
+
+// num
+// =
+// 29
+// num=29
+// Check divisors from 2 to 14 (num / 2):
+// 29 % 2 ≠ 0
+// 29 % 3 ≠ 0
+// 29 % 4 ≠ 0
+// 29 % 5 ≠ 0
+// 29 % 6 ≠ 0
+// 29 % 7 ≠ 0
+// 29 % 8 ≠ 0
+// 29 % 9 ≠ 0
+// 29 % 10 ≠ 0
+// 29 % 11 ≠ 0
+// 29 % 12 ≠ 0
+// 29 % 13 ≠ 0
+// 29 % 14 ≠ 0
+// Since none of these numbers divide 29 without a remainder, 29 is confirmed as a prime number. However, we performed 13 checks.
+
+// Using Math.sqrt(num)
+// If we check up to the square root of the number, we would iterate through fewer steps:
+
+// num
+// =
+// 29
+// num=29
+// sqrt(num)
+// ≈
+// 5.39
+// sqrt(num)≈5.39
+// Check divisors from 2 to 5:
+// 29 % 2 ≠ 0
+// 29 % 3 ≠ 0
+// 29 % 4 ≠ 0
+// 29 % 5 ≠ 0
+// Since none of these numbers divide 29 without a remainder, 29 is confirmed as a prime number. We only performed 4 checks, which is much more efficient.
--- Workspace.java
+++ Workspace.java
@@ -1,9 +1,32 @@
 import java.util.*;
-class workspace{
+class Workspace{
     public static void main(String ae[]){
-        int arr[]={7,8,9,4,344,5,57,2};
-        if(arr[0]>1){
-            
+        String str="abaabaab";
+        String lp="";
+        int l=str.length();
+        int ml=0;
+        for(int i=0;i<l;i++){
+            for(int j=i+1;j<l;j++){
+                String sb=str.substring(i,j);
+                if(isPlalindram(sb) && sb.length()>ml){
+                    ml=sb.length();
+                    lp=sb;
+                }
+
+            }
         }
+    System.out.println(lp);
     }
+        public static boolean isPlalindram(String sb){
+            int l=0;
+            int r=sb.length()-1;
+            while(l<r){
+                if(sb.charAt(l)!=sb.charAt(r)){
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
 }
